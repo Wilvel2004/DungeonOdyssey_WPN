37adbbeec6ed929a0cbf4d6f95324149cfaf8f88
Wilvel2004 <wilvelpineda@gmail.com>
2024-12-07 19:27:48 +0100

    parkour 1 de la zona 2

--- Entities/Bosses/Golem/States/Follow.gd
+++ Entities/Bosses/Golem/States/Follow.gd
@@ -1,9 +1,10 @@
 extends State
- 
+
 func enter():
 	super.enter()
 	owner.set_physics_process(true)
 	animation_player.play("idle")
+	await get_tree().create_timer(1.0).timeout
  
 func exit():
 	super.exit()
@@ -11,8 +12,7 @@ func exit():
  
 func transition():
 	var distance = owner.direction.length()
- 
-	if distance < 50:
+	if distance < 40:
 		get_parent().change_state("MeleeAttack")
 	elif distance > 200:
 		get_parent().change_state("HomingMissile")
